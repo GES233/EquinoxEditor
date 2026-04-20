1bd151c0f03da9280c9aa8953fb4070bcb47d276
RoastedChestnut <chestnut.roasted@outlook.com>
2026-04-20 18:53:28 +0800

    chore(util): add memory leak warning to Attrs.normalize
    Update the documentation for Equinox.Util.Attrs and add a TODO note regarding the potential memory leak risk in the normalize function. Using String.to_atom/1 on external input can exhaust the atom table, so a whitelist approach using struct fields is suggested for future implementation.

--- kernel/lib/equinox/util/attrs.ex
+++ kernel/lib/equinox/util/attrs.ex
@@ -1,9 +1,13 @@
 defmodule Equinox.Util.Attrs do
-  @moduledoc "因为数据的来源之一是序列化而必需的一些函数。"
+  @moduledoc """
+  数据的来源之一是序列化而必需的工具函数。
+  """
 
   @type attributes :: map() | keyword()
 
   @doc "标准化 Attrs 。"
+  # TODO: String.to_atom(k) 可能造成内存泄露风险
+  # 需要引入 struct 的 fields 或其他手段作为可被转化为原子的白名单
   def normalize(map_or_kw) do
     map_or_kw
     |> Enum.into(%{})
